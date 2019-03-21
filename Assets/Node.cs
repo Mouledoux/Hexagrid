85c50dd2b1cc43517ed46f04a62373834257cdab
@rootAccess <EricMouledoux@Gmail.com>
2019-03-21 17:57:35 -0500

    Hexagrid!

---
+++ Assets/Node.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Eric
+{
+    public class Node : MonoBehaviour, IPointerClickHandler, IPointerUpHandler
+    {
+        private List<Node> neighbors = new List<Node>();
+
+        Vector3 originalScale;
+        private void Start()
+        {
+            originalScale = transform.localScale;
+        }
+
+        private void Update()
+        {
+            transform.localScale = Vector3.Lerp(transform.localScale, originalScale, Time.deltaTime);
+        }
+        public int AddNeighbor(Node newNeighbor)
+        {
+            if(newNeighbor == null)
+                return -1;
+
+            if(!neighbors.Contains(newNeighbor))
+                neighbors.Add(newNeighbor);
+
+            if(!newNeighbor.GetNegibors().Contains(this))
+                newNeighbor.AddNeighbor(this);
+
+            return 0;
+        }
+
+        public List<Node> GetNegibors()
+        {
+            List<Node> myNeighbors = neighbors;
+            return myNeighbors;
+        }
+
+        public IEnumerator PropigateOut(Node root, List<Node> affected, int maxProp)
+        {
+            if(affected == null)
+            {
+                affected = new List<Node>();
+                print("New List!");
+            }
+
+            else if(affected.Count >= maxProp)
+                yield break;
+
+            foreach(Node n in root.neighbors)
+            {
+                if(!affected.Contains(n))
+                {
+                    //n.gameObject.GetComponent<Renderer>().material.color =
+                    //    new Color(1, (affected.Count % 2) / 100f, affected.Count / 100f);
+
+                    n.transform.localScale = new Vector3(0.9f, 0.1f, 0.9f);
+                    affected.Add(n);
+                }
+                //yield return new WaitForEndOfFrame();
+            }
+
+            yield return null;
+
+            foreach(Node n in root.neighbors)
+                foreach(Node o in n.neighbors)
+                    if(!affected.Contains(o))
+                        n.StartCoroutine(PropigateOut(n, affected, maxProp));
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            StartCoroutine(PropigateOut(this, null, 100));
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            //StartCoroutine(PropigateOut(this, null, 100));
+        }
+    }
+}
---
+++ Assets/SpawnGrid.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Eric
+{
+    public class SpawnGrid : MonoBehaviour
+    {
+        public List<GameObject> lowLand, medLand, highLand, Special;
+        public int rows, cols;
+        public Node[,] gridNodes;
+
+        IEnumerator Start()
+        {
+            gridNodes = new Node[cols, rows];
+
+            for(int i = 0; i < cols; i++)
+            {
+                for (int j = 0; j < rows; j++)
+                {
+                    float perlinScale = 16f;
+                    float perlinHeight = Mathf.PerlinNoise(((float)i/(float)cols) * perlinScale,((float)j/(float)rows) * perlinScale);
+                    
+                    GameObject gridCell = null;
+
+                    if(Random.value >= 0.99f)
+                        gridCell = Instantiate(Special[Random.Range(0, Special.Count)]) as GameObject;
+
+                    else if(perlinHeight >= 0.8f)
+                        gridCell = Instantiate(highLand[Random.Range(0, highLand.Count)]) as GameObject;
+
+                    else  if(perlinHeight >= 0.4f)
+                        gridCell = Instantiate(medLand[Random.Range(0, medLand.Count)]) as GameObject;
+
+                    else
+                        gridCell = Instantiate(lowLand[Random.Range(0, lowLand.Count)]) as GameObject;
+
+
+                    gridCell.name = $"[{i}, {j}]";
+                    gridCell.transform.parent = transform;
+            
+                    gridCell.transform.localPosition = new Vector3(i, perlinHeight * 4, j + (i%2==0?.5f:0f));
+                    gridCell.transform.Rotate(Vector3.up, 30);
+                    //Vector3 scale = new Vector3(0.9f, perlinHeight * 16, 0.9f);
+                    //gridCell.transform.localScale = scale;
+
+                    //Color perlinColor = new Color(scale.y / 3f, 1, scale.y / 5f);
+                    //gridCell.GetComponent<Renderer>().material.color = perlinColor;
+
+                    gridNodes[i, j] = (gridCell.AddComponent<Node>());
+                    
+                    if(j > 0)
+                        gridNodes[i, j].AddNeighbor(gridNodes[i, j-1]);
+
+                    if(i > 0)
+                    {
+                        gridNodes[i, j].AddNeighbor(gridNodes[i-1, j]);
+
+                        int nextJ = j+(i%2==0?1:-1);
+                        if(nextJ >= 0 && nextJ < rows)
+                            gridNodes[i, j].AddNeighbor(gridNodes[i-1, nextJ]);
+                    }
+                    
+                    //yield return null;
+                }
+                yield return null;
+            }
+
+            yield return null;
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+}
