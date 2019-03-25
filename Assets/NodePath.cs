1d2bd294934f77a50ea7a222eac64e9a1ee6bca5
@rootAccess <EricMouledoux@Gmail.com>
2019-03-25 14:18:07 -0500

    Node path class

---
+++ Assets/NodePath.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public sealed class NodePath
+{
+    private Node _startNode, _endNode;
+
+    private Queue<Node> _closedQueue;
+    private Stack<Node> _openStack;
+
+    public Queue<Node> GetNodePath(Node start, Node goal)
+    {
+        
+
+        return _closedQueue;
+    }
+
+
+
+
+
+    private float GetHScore(Node aNode)
+    {
+        return(Node.Distance(aNode, _endNode));
+    }
+
+    private float GetGScore(Node aNode)
+    {
+        return(Node.Distance(aNode, _startNode));
+    }
+
+    private float GetFScore(Node aNode)
+    {
+        return(GetHScore(aNode) + GetGScore(aNode));
+    }
+}
