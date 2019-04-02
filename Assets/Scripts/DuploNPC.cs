e2e7e7168b7778c8004a58dd9209499a23492c1e
@rootAccess <EricMouledoux@Gmail.com>
2019-04-02 10:38:59 -0500

    duplo script

---
+++ Assets/Scripts/DuploNPC.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DuploNPC : MonoBehaviour
+{
+    private Stack<TraversableNode> path;
+
+    void Update()
+    {
+        if(path != null && path.Count > 0)
+        {
+            Vector3 dir = path.Peek().transform.position - transform.position;
+            transform.Translate(dir* Time.deltaTime);
+
+            if(Vector3.Distance(transform.position, path.Peek().transform.position) <= 0.05f)
+            {
+                path.Pop();
+            }
+        }
+    }
+}
