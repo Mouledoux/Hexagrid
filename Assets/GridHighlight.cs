eb92f930697f2d899819634e8e01e101964d095e
@rootAccess <EricMouledoux@Gmail.com>
2019-04-25 13:45:27 -0500

    messing with shader graph

---
+++ Assets/GridHighlight.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridHighlight : MonoBehaviour
+{
+    Material mat;
+    Vector3 mp;
+    RaycastHit rayHit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mat = GetComponentInChildren<Renderer>().sharedMaterial;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out rayHit, 64);
+        mp = rayHit.point;
+        mat.SetVector("_MousePos", transform.position);
+        mp.y = Vector3.Distance(transform.position, mp);
+    }
+}
