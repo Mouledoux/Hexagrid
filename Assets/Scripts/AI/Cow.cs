5f95b4ff84742e0f3363d93042803fdb95205390
@rootAccess <EricMouledoux@Gmail.com>
2019-06-11 12:02:57 -0500

    removed rts camera

--- Assets/Scripts/AI/Cow.cs
+++ Assets/Scripts/AI/Cow.cs
@@ -30,6 +30,17 @@ public class Cow : MonoBehaviour
     public IEnumerator NewRandomPathRoutine()
     {
         yield return new WaitForSeconds(Random.Range(0f, waitTime));
+
+        foreach(Node na in navAgent.currentPositionNode.GetNeighborhood())
+        {
+            NodeNavAgent[] agentsInRange = na.GetInformation<NodeNavAgent>();
+            if(agentsInRange.Length > 0)
+            {
+                Node[] potentialTargets = agentsInRange[0].currentPositionNode.GetNeighborhoodLayers(2, 2);
+                navAgent.goalPositionNode = potentialTargets[Random.Range(0, potentialTargets.Length)] as TraversableNode;
+            }
+        }
+
         navAgent.SetRandomDestination(2, 2);
     }
 }
