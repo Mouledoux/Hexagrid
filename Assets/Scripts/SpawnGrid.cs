8eaf9267cc06a2b0a4492c309133796cdc1db605
@rootAccess <EricMouledoux@Gmail.com>
2019-07-01 18:04:12 -0500

    new Map genreation!

--- Assets/Scripts/SpawnGrid.cs
+++ Assets/Scripts/SpawnGrid.cs
@@ -5,14 +5,15 @@ using System.Collections.Generic;
 public class SpawnGrid : MonoBehaviour
 {
     public int rows, cols;
+    public string seed;
 
-    public int perlinSeed;
+    private int perlinSeed => (seed.GetHashCode() >> 16);
     public float perlinScale = 8f;
     public float perlinHeightMod = 0.2f;
 
     public float biomeSeedMod = 0.5f;
 
-
+    public Texture2D mapTexture;
     
     public bool edgesAreWalls;
     public List<GameObject> outerWall, lowLand, medLand, highLand, Special;
@@ -23,7 +24,8 @@ public class SpawnGrid : MonoBehaviour
         rows = Mathf.Abs(rows);
         cols = Mathf.Abs(cols);
 
-        GenerateNewMap();
+        //GenerateNewMap();
+        NewTextureMap();
     }
 
     private void Update()
@@ -35,20 +37,76 @@ public class SpawnGrid : MonoBehaviour
 
     bool ClearBoard()
     {
-        if(gridNodes == null || gridNodes[rows-1, cols-1] == null)
+        if(gridNodes == null)
             return false;
+
+        foreach (TraversableNode node in gridNodes)
+        {
+            Destroy(node.gameObject);
+        }
+
+        return true;
+    }
+
+    public void NewTextureMap()
+    {
+        ClearBoard();
+        gridNodes = new TraversableNode[cols, rows];
+
+        GameObject gridCell;
+        int txWidth = mapTexture.width;
+        int txHeight = mapTexture.height;
 
         for(int i = 0; i < cols; i++)
         {
             for (int j = 0; j < rows; j++)
             {
-                if(gridNodes[i, j] != null)
+                int coX, coY;
+                coX = (int)((float)((float)i/(float)cols) * txWidth);
+                coY = (int)((float)((float)j/(float)rows) * txHeight);
+
+                int hexOffset = (i % 2);
+                float h, s, v;
+                Color.RGBToHSV(mapTexture.GetPixel(coX, coY), out h, out s, out v);
+
+
+                if(v > 0)
                 {
-                    Destroy(gridNodes[i, j].gameObject);
+                    gridCell = Instantiate(outerWall[0]) as GameObject;
+
+                    gridCell.name = $"[{i}, {j}]";
+                    gridCell.transform.parent = transform;
+                    gridCell.transform.localPosition = new Vector3(((-cols / 2) + i) * 0.85f, 0, ((-rows / 2) + j) + (hexOffset * 0.5f));
+                    gridCell.transform.Rotate(Vector3.up, 30);
+                    gridCell.transform.localScale = Vector3.one + Vector3.up * v * 8f;
+
+                    gridNodes[i, j] = (gridCell.GetComponent<TraversableNode>());
+                    gridNodes[i, j] = gridNodes[i, j] == null ? gridCell.AddComponent<TraversableNode>() : gridNodes[i, j];
+                    
+                    gridNodes[i, j].xCoord = i;
+                    gridNodes[i, j].yCoord = j;
+                    gridNodes[i, j].travelCost = 1;
+                    gridNodes[i, j].isTraversable = true;
+                    
+                    if(j > 0)
+                    {
+                        gridNodes[i, j].AddNeighbor(gridNodes[i, j - 1]);
+                    }
+
+                    if(i > 0)
+                    {
+                        gridNodes[i, j].AddNeighbor(gridNodes[i - 1, j]);
+
+                        int nextJ = j + (hexOffset * 2 - 1);
+
+                        if(nextJ >= 0 && nextJ < rows)
+                        {
+                            gridNodes[i, j].AddNeighbor(gridNodes[i - 1, nextJ]);
+                        }
+                    }
                 }
             }
         }
-        return true;
     }
 
     [ContextMenu("NewMap")]
@@ -71,7 +129,7 @@ public class SpawnGrid : MonoBehaviour
                 float height = (int)(perlinHeight * perlinScale) * perlinHeightMod;
                 
 
-                bool isWall = (i == 0 || j == 0 || i == cols-1 || j == rows-1) && edgesAreWalls;
+                bool isWall = edgesAreWalls && (i == 0 || j == 0 || i == cols-1 || j == rows-1);
                 Vector3 scale = isWall ? new Vector3(1f, 16f, 1f) : new Vector3(1f, 1f,  1f);
 
 
@@ -118,10 +176,10 @@ public class SpawnGrid : MonoBehaviour
 
                     gridCell.name = $"[{i}, {j}]";
                     gridCell.transform.parent = transform;
-                    gridCell.transform.localPosition = new Vector3(((-cols / 2) + i) * 0.85f, height, ((-rows / 2) + j) + (hexOffset * 0.5f));
-                    //gridCell.transform.localPosition = new Vector3(((-cols / 2) + i), 0, ((-rows / 2) + j * 1.1f) + (hexOffset * 0.5f));
+                    //gridCell.transform.localPosition = new Vector3(((-cols / 2) + i) * 0.85f, height, ((-rows / 2) + j) + (hexOffset * 0.5f));
+                    gridCell.transform.localPosition = new Vector3(((-cols / 2) + i) * 0.85f, 0, ((-rows / 2) + j) + (hexOffset * 0.5f));
                     gridCell.transform.Rotate(Vector3.up, 30);
-                    gridCell.transform.localScale = Vector3.one;// + Vector3.up * height * 16;
+                    gridCell.transform.localScale = Vector3.one + Vector3.up * height * 8;
 
                     gridNodes[i, j] = (gridCell.GetComponent<TraversableNode>());
                     gridNodes[i, j] = gridNodes[i, j] == null ? gridCell.AddComponent<TraversableNode>() : gridNodes[i, j];
